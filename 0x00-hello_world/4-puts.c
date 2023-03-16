b059f85cf89affe2e69e3276fd05270c009bc743
notyous <ysrakd@gmail.com>
2023-03-16 08:10:36 -0700

    C tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print string
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
