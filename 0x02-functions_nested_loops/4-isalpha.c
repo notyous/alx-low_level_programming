ba4315792bafe475e16a5b731cc9c91c8d30ee00
notyous <ysrakd@gmail.com>
2023-03-21 11:18:27 -0700

    4-isalpha.c task

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _isalpha - Return 1 if @c is a letter, lowercase or uppercase
+ * Description: Checks for alphabetic characters
+ * @c: Contains value to be compared
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
+
+int _isalpha(int c)
+{
+	/* ASCII value of upper case letters is between 65 && 90 */
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+
+	else
+	{
+		/* ASCII value of lower case letters is between 97 && 122 */
+		if (c >= 97 && c <= 122)
+		{
+			return (1);
+		}
+
+		else
+		{
+			return (0);
+		}
+	}
+}
