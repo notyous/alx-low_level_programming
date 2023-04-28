a23d4b4519b0748b3b8c65821453bd5fc5c15594
notyous <ysrakd@gmail.com>
2023-04-28 06:41:59 -0700

    all

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* free_listint2 - frees listint_t and set head to NULL
+* @head: head node
+*
+* Return: void
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp, *next;
+
+	if (!head)
+		return;
+	tmp = *head;
+	while (tmp)
+	{
+		next = tmp->next;
+		free(tmp);
+		tmp = next;
+	}
+	*head = 0;
+}
